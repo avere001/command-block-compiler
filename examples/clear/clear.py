89325d1cef90a846112c5d0058abf26b8d368aa4
Adam V <adam@example.com>
2020-09-23 00:04:14 -0700

    Update clear script

--- examples/clear/clear.py
+++ examples/clear/clear.py
@@ -1,16 +1,16 @@
 set_prefix('CL')
 set_objective('vars')
 
-`summon ArmorStand ~2 ~ ~-1 {CustomName:Deleter,NoGravity:1b,Marker:1b}`
+`summon minecraft:armor_stand ~2 ~ ~-1 {CustomName:Deleter,NoGravity:1b,Marker:1b}`
 `tellraw @p ["",{"text":"Clearing.."}]`
-while `execute @e[name=Deleter] ~ ~ ~ testforblock ~ ~ ~ command_block` do
-    `execute @e[name=Deleter] ~ ~ ~ fill ~ ~ ~ ~ ~ ~ air`
-    `execute @e[name=Deleter] ~ ~ ~ fill ~ ~ ~ ~ ~ ~125 air 0 replace chain_command_block`
-    `execute @e[name=Deleter] ~ ~ ~ kill @e[r=1,type=ArmorStand,name=!Deleter]`
-    `execute @e[name=Deleter] ~ ~ ~ tp @e[name=Deleter] ~1 ~ ~`
+while `execute as @e[name=Deleter] at @s run testforblock ~ ~ ~ command_block` do
+    `execute as @e[name=Deleter] at @s run fill ~ ~ ~ ~ ~ ~ air`
+    `execute as @e[name=Deleter] at @s run fill ~ ~ ~ ~ ~ ~125 air 0 replace chain_command_block`
+    `execute as @e[name=Deleter] at @s run kill @e[r=1,type=minecraft:armor_stand,name=!Deleter]`
+    `execute as @e[name=Deleter] at @s run tp @e[name=Deleter] ~1 ~ ~`
 end
 `kill @e[name=Deleter]`
 `tellraw @e[r=25] ["",{"text":"Finished Clearing!"}]`
-`kill @e[type=ArmorStand,r=20]`
+`kill @e[type=minecraft:armor_stand,r=20]`
 # `summon Creeper ~ ~1 ~ {ignited:1,NoGravity:1,ExplosionRadius:4}`
 `fill ~-5 ~ ~-4 ~5 ~ ~7 air`
